50533b8c604c6a9ca094920dbe74c80db57f041f
seoulbanu <121330150+seoulbanu@users.noreply.github.com>
2022-12-24 23:45:45 +0530

    Add files via upload

---
+++ python lab 2  output.py
@@ -0,0 +1,20 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:16:59) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/LENOVO/Documents/python lab 2.py =============
+value of x: 50
+value of y: 10
+value of x: 50
+value of y: 10
+value of x: 50
+value of y: 10
+value of x: 50
+value of y: 10
+a after circulating: [4, 5, 6, 1, 2, 3]
+b after circulating: [4, 5, 6, 1, 2, 3]
+enter x1:4
+enter x2:6
+enter y1:0
+enter y2:6
+distance between (4, 6) and (0, 6) is: 6.324555320336759
+>>>  
